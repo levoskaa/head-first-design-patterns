b50579bd95d6a4c39065c52f70a3d579e992ea85
levoskaa <47329402+levoskaa@users.noreply.github.com>
2024-04-26 11:52:04 +0200

    feat(weather-station): add display elements

---
+++ WeatherStation/src/main/java/com/github/levoskaa/StatisticsDisplay.java
@@ -0,0 +1,30 @@
+package com.github.levoskaa;
+
+public class StatisticsDisplay implements Observer, DisplayElement {
+
+    private float maxTemp = 0.0f;
+    private float minTemp = 200;
+    private float tempSum = 0.0f;
+    private int numReadings;
+    private WeatherData weatherData;
+
+    public StatisticsDisplay(WeatherData weatherData) {
+        this.weatherData = weatherData;
+        weatherData.registerObserver(this);
+    }
+
+    @Override
+    public void update(float temperature, float humidity, float pressure) {
+        tempSum += temperature;
+        numReadings++;
+        maxTemp = Math.max(maxTemp, temperature);
+        minTemp = Math.min(minTemp, temperature);
+        display();
+    }
+
+    @Override
+    public void display() {
+        System.out.printf("Avg/Max/Min temperature = %f/%f/%f%n",
+                tempSum / numReadings, maxTemp, minTemp);
+    }
+}
